fafb2ec758d91731be6970377b91e46cb62aa28f
Ebrahim Alshaker <alshaker@proton.me>
2025-11-08 22:33:16 +0300

    Outline student services

---
+++ src/services/studentServices.js
@@ -0,0 +1,16 @@
+import connectDatabase from "../config/db";
+import Student from "../models/studentModel";
+
+const getAllStudentsService = async () => {};
+const createStudentService = async () => {};
+const getStudentByIdService = async () => {};
+const updateStudentByIdService = async () => {};
+const deleteStudentByIdService = async () => {};
+
+module.exports = {
+  getAllStudentsService,
+  createStudentService,
+  getStudentByIdService,
+  updateStudentByIdService,
+  deleteStudentByIdService,
+};
