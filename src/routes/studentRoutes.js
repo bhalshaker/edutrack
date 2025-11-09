72f528a4036ca5a5c974258d39da2e776be63e1e
Ebrahim Alshaker <alshaker@proton.me>
2025-11-09 22:21:58 +0300

    Add validation to routes

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -7,11 +7,31 @@ import {
   updateStudentById,
   deleteStudentById,
 } from "../controllers/studentController";
+import {
+  createStudentSchema,
+  studentIdSchema,
+  updateStudentSchema,
+} from "../schemas/studentSchemas";
+
+import { validateSchemas } from "../utility/schemaValidation";
 
 Router.get("/", getAllStudents);
-Router.post("/", createStudent);
-Router.get("/:studentId", getStudentById);
-Router.patch("/:studentId", updateStudentById);
-Router.delete("/:studentId", deleteStudentById);
+Router.post("/", validateSchemas(createStudentSchema, "body"), createStudent);
+Router.get(
+  "/:studentId",
+  validateSchemas(studentIdSchema, "params"),
+  getStudentById
+);
+Router.patch(
+  "/:studentId",
+  validateSchemas(studentIdSchema, "params"),
+  validateSchemas(updateStudentSchema, "body"),
+  updateStudentById
+);
+Router.delete(
+  "/:studentId",
+  validateSchemas(studentIdSchema, "params"),
+  deleteStudentById
+);
 
 module.exports = Router;
