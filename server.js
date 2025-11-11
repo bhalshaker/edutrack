42d4107e4bd8e39937b16c4a16dbce301b41cd07
Ebrahim Alshaker <alshaker@proton.me>
2025-11-11 21:53:11 +0300

    Fix config import and env log

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-import dotenv from "dotenv";
 import express from "express";
 import { connectDatabase } from "./src/config/db.js";
 import config from "./src/config/config.js";
 import { StudentRouter } from "./src/routes/studentRoutes.js";
 
-// Load the environment variables from .env file
-dotenv.config();
 // Connect to the database server
 connectDatabase();
 // Initilize Express application
